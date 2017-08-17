c523a12491678af2e422c9386ab07ed2fddbe5ee
zwStar <979904565@qq.com>
2017-08-17 10:33:33 +0800

    zw

--- utils/config.js
+++ utils/config.js
@@ -3,7 +3,8 @@
  */
 export default {
     allowOrigin:'http://127.0.0.1:8080',    //跨域地址
-    db:'mongodb://zwStar:adgjmp123@119.23.109.7:27017/example',                          //数据存储的库
+    //db:'mongodb://zwStar:adgjmp123@119.23.109.7:27017/example',                          //数据存储的库
+    db:'mongodb://localhost:27017/example',
     testdb:'mongodb://zwVic:adgjmp123@119.29.82.47:27017/testdb',
     secret:"zwStar"
 }
